73c343e143613c14d980e845b4a649080004ecc1
Jayant Gupta <jayantgupta55550@gmail.com>
2026-07-01 12:17:50 +0530

    Update CORS for Netlify

--- src/main/java/com/cs/rd/config/WebConfig.java
+++ src/main/java/com/cs/rd/config/WebConfig.java
@@ -7,12 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOriginPatterns("*")
-		.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-		.allowedHeaders("*")
-		.allowCredentials(true)
-		.maxAge(3600);
-	}
+	 public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://rdsystm.netlify.app/"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
 }
